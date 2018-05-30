cfe2efd611ab9bf3267b5b008b3fce186db917cf
Michael-Bello <michaelbello.cj@gmail.com>
2018-05-30 15:31:17 +0100

    FP Nav & Anime
    Final Project Navigation bars working and animate.css (slide left & pulse) added to the header (Michael)

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 
-  $('#toggle-btn').on('click', toggleSidebar)
+  $('.toggle-btn').on('click', toggleSidebar)
 
   function toggleSidebar() {
-    document.getElementById('toggle-btn').classList.toggle('active');
-    document.getElementById('sidebar').classList.toggle('active');
+    document.querySelector('.toggle-btn').classList.toggle('active');
+    document.querySelector('.sidebar').classList.toggle('active');
   }
+
+  var $header = $('#header');
+
+  setTimeout(() => {
+    $header.removeClass().addClass('animated infinite pulse');
+  }, 1000);
 })
